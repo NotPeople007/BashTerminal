5652dde7931c3e73b3874f1900dfcc2abbe96609
DEN <denkra2003.dd@gmail.com>
2019-04-08 17:42:05 +0300

    Create software manager

--- lib/SoftwareManager.sh
+++ lib/SoftwareManager.sh
@@ -1,3 +1,24 @@
 #! /usr/bin/bash
 
-echo "Software manager"
+SMMenu(){
+  echo -e "1. Install\n2. Search\n3. Remove\n4. Update\n5. Repolist\n0. Exit"
+  read choice
+}
+
+Install(){
+  read -p "Enter packet to install ===> " packetname 
+  yum install $packetname
+}
+
+smExit=true
+
+while [ $smExit == true ]
+do
+  clear
+  SMMenu;
+  case $choice in
+    1) Install; ;;
+    0) echo "Back to main menu."; let smExit=false; ;;
+    *) echo "Wrong choice!"; ;;
+  esac
+done
