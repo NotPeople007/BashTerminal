7d879f28177ac838308eb6732948f076cbb6d3ac
DEN <denkra2003.dd@gmail.com>
2019-04-08 17:33:11 +0300

    Create skeleton

--- BashTerminal.sh
+++ BashTerminal.sh
@@ -1,1 +1,20 @@
 #! /usr/bin/bash
+clear
+MainMenu(){
+  echo -e "1. Software manager\n0. Exit";
+  read choice
+}
+
+
+exit=true
+
+while [ $exit == true ]
+do
+  MainMenu;
+  case $choice in
+    1) source lib/SoftwareManager.sh; ;;
+    0) echo "Bye!"; let exit=false; ;;
+    *) echo "Wring choice!"; ;;
+  esac
+
+done
